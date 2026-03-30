f2ccfff18ce9026561536a603b8aaa3cf0d3f362
Shinji Kaneko <minimanberry@gmail.com>
2026-03-30 13:37:44 +0200

    1st

---
+++ src/index.ts
@@ -0,0 +1,74 @@
+import {
+  AssetManifest,
+  AssetType,
+  Mesh,
+  MeshBasicMaterial,
+  PlaneGeometry,
+  SessionMode,
+  SRGBColorSpace,
+  AssetManager,
+  World,
+} from "@iwsdk/core";
+
+import {
+  AudioSource,
+  DistanceGrabbable,
+  MovementMode,
+  Interactable,
+  PanelUI,
+  PlaybackMode,
+  ScreenSpace,
+} from "@iwsdk/core";
+
+import { EnvironmentType, LocomotionEnvironment } from "@iwsdk/core";
+
+const assets: AssetManifest = {
+  environmentDesk: {
+    url: "./gltf/environmentDesk/environmentDesk.gltf",
+    type: AssetType.GLTF,
+    priority: "critical",
+  },
+
+  model: {
+    url: "./model.glb",
+    type: AssetType.GLTF,
+    priority: "critical",
+  },
+};
+
+World.create(document.getElementById("scene-container") as HTMLDivElement, {
+  assets,
+  xr: {
+    sessionMode: SessionMode.ImmersiveVR,
+    offer: "always",
+    // Optional structured features; layers/local-floor are offered by default
+    features: { handTracking: true, layers: true },
+  },
+  features: {
+    locomotion: { useWorker: true },
+    grabbing: true,
+    physics: false,
+    sceneUnderstanding: false,
+    environmentRaycast: false,
+  },
+}).then((world) => {
+  const { camera } = world;
+
+  camera.position.set(-4, 1.5, -6);
+  camera.rotateY(-Math.PI * 0.75);
+
+  const { scene: envMesh } = AssetManager.getGLTF("environmentDesk")!;
+  envMesh.rotateY(Math.PI);
+  envMesh.position.set(0, -0.1, 0);
+  world
+    .createTransformEntity(envMesh)
+    .addComponent(LocomotionEnvironment, { type: EnvironmentType.STATIC });
+
+  const { scene: modelMesh } = AssetManager.getGLTF("model")!;
+
+  modelMesh.position.set(1.2, 0.85, -1.8);
+
+  world.createTransformEntity(modelMesh).addComponent(DistanceGrabbable, {
+    movementMode: MovementMode.MoveFromTarget,
+  });
+});
