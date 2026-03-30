ec0560ac8b7a025b5ec65b8cfeeb309b9963701f
Shinji Kaneko <minimanberry@gmail.com>
2026-03-30 16:46:37 +0200

    add move up/down

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,22 @@ import {
   AssetManifest,
   AssetType,
   CanvasTexture,
+  InputComponent,
   Mesh,
   MeshBasicMaterial,
   PlaneGeometry,
   SessionMode,
   AssetManager,
   World,
+  createSystem,
 } from "@iwsdk/core";
 
 import {
   AudioSource,
   DistanceGrabbable,
+  LocomotionSystem,
   MovementMode,
+  TeleportSystem,
   Interactable,
   PanelUI,
   PlaybackMode,
@@ -26,6 +30,124 @@ import { createPointingArrow } from "./createPointingArrow";
 import { createUserAvatar } from "./createUserAvatar";
 import { enableMultiplayer } from "./enableMultiplayer";
 import { enforceUniformScale } from "./enforceUniformScale";
+
+const CAMERA_VERTICAL_SPEED = 3;
+const THUMBSTICK_DEADZONE = 0.1;
+
+type InternalLocomotionSystem = {
+  locomotor?: {
+    position: {
+      y: number;
+    };
+    targetPosition?: {
+      y: number;
+    };
+    engine?: {
+      gravity: number;
+      playerPosition: {
+        y: number;
+      };
+      playerVelocity?: {
+        y: number;
+      };
+      isGrounded?: boolean;
+    };
+  };
+};
+
+const getLocomotionSystem = (
+  world: World,
+): InternalLocomotionSystem | undefined => {
+  return (
+    world as unknown as {
+      getSystem: (system: typeof LocomotionSystem) => unknown;
+    }
+  ).getSystem(LocomotionSystem) as InternalLocomotionSystem | undefined;
+};
+
+const getVerticalThumbstickInput = (world: World): number => {
+  const rightGamepad = world.input.gamepads.right;
+
+  if (rightGamepad?.getButtonPressed(InputComponent.Squeeze)) {
+    const rightAxes = rightGamepad.getAxesValues(InputComponent.Thumbstick);
+
+    if (rightAxes && Math.abs(rightAxes.y) > THUMBSTICK_DEADZONE) {
+      return -rightAxes.y;
+    }
+  }
+
+  return 0;
+};
+
+class VerticalControllerMovementSystem extends createSystem() {
+  private teleportDisabled = false;
+  private lockedHeight: number | null = null;
+
+  update(delta: number): void {
+    if (!this.teleportDisabled) {
+      const teleportSystem = (
+        this.world as unknown as {
+          getSystem: (system: typeof TeleportSystem) => unknown;
+          unregisterSystem: (system: typeof TeleportSystem) => void;
+        }
+      ).getSystem(TeleportSystem);
+
+      if (teleportSystem) {
+        (
+          this.world as unknown as {
+            unregisterSystem: (system: typeof TeleportSystem) => void;
+          }
+        ).unregisterSystem(TeleportSystem);
+        this.teleportDisabled = true;
+      }
+    }
+
+    const verticalInput = getVerticalThumbstickInput(this.world);
+    const locomotionSystem = getLocomotionSystem(this.world);
+    const locomotor = locomotionSystem?.locomotor;
+    const engine = locomotor?.engine;
+
+    if (engine) {
+      engine.gravity = 0;
+    }
+
+    if (!locomotor) {
+      this.lockedHeight = null;
+      return;
+    }
+
+    if (verticalInput !== 0) {
+      const heightDelta = verticalInput * CAMERA_VERTICAL_SPEED * delta;
+      const nextHeight = locomotor.position.y + heightDelta;
+
+      this.lockedHeight = nextHeight;
+    }
+
+    if (this.lockedHeight === null) {
+      this.lockedHeight = locomotor.position.y;
+    }
+
+    locomotor.position.y = this.lockedHeight;
+
+    if (locomotor.targetPosition) {
+      locomotor.targetPosition.y = this.lockedHeight;
+    }
+
+    if (engine) {
+      engine.playerPosition.y = this.lockedHeight;
+
+      if (engine.playerVelocity) {
+        engine.playerVelocity.y = 0;
+      }
+
+      if (engine.isGrounded !== undefined) {
+        engine.isGrounded = false;
+      }
+    }
+
+    this.world.player.position.y = this.lockedHeight;
+  }
+}
 
 const createFloorGridTexture = (): CanvasTexture => {
   const canvas = document.createElement("canvas");
@@ -82,7 +204,7 @@ World.create(document.getElementById("scene-container") as HTMLDivElement, {
     features: { handTracking: true, layers: true },
   },
   features: {
-    locomotion: { useWorker: true },
+    locomotion: { useWorker: false },
     grabbing: true,
     physics: false,
     sceneUnderstanding: false,
@@ -93,6 +215,8 @@ World.create(document.getElementById("scene-container") as HTMLDivElement, {
 
   camera.position.set(-4, 1.5, -6);
   camera.rotateY(-Math.PI * 0.75);
+
+  world.registerSystem(VerticalControllerMovementSystem, { priority: 6 });
 
   createUserAvatar(world);
 
